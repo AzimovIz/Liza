7cb8de3574f4bc375f2bdb02cc7b009d13c30fa0
AzimovIz <66750886+AzimovIz@users.noreply.github.com>
2021-01-23 01:06:24 +0300

    Add port selector

--- COM.py
+++ COM.py
@@ -1,7 +1,17 @@
 import serial
+import subprocess
+
+
+p = subprocess.check_output("python -m serial.tools.list_ports")
+ports = p.replace("\n", " ").split()
+for i in range(len(ports)):
+    print("port " + str(i) + " : " + ports[i])
+port_n = input('Input port number: ')
+print("Your select : " + str(ports[int(port_n)]))
+port = ports[int(port_n)]
 
 ser = serial.Serial(
-    port = 'COM2',
+    port = port,
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
