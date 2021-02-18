af261bf8511ad543bf4cffc435ebb4e264a70265
AzimovIz <66750886+AzimovIz@users.noreply.github.com>
2021-02-18 22:51:08 +0300

    none

--- pars.py
+++ pars.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-
+urls = ["http://anime.reactor.cc/tag/Anime+Ero+Ass/new",
+        "http://anime.reactor.cc/tag/Anime+Ero+Pantsu/new",
+        "http://anime.reactor.cc/tag/Anime+Ero+Gifs/new"]
 
 def pars(log=False, url = 'http://reactor.cc'):
     r = requests.get(url)
@@ -8,8 +10,10 @@ def pars(log=False, url = 'http://reactor.cc'):
         print(f"[log] Requests status: {r.status_code}")
     soup = BeautifulSoup(r.text)
     item = soup.find_all('a', {'class': 'prettyPhotoLink'})
+    item2 = soup.find_all('a', {'class': 'video_gif_source'})
+    item[len(item):] = item2
 
-    links = []
+    links=[]
     for i in item:
         links.append(i.get('href'))
         if log:
@@ -17,16 +21,18 @@ def pars(log=False, url = 'http://reactor.cc'):
     links.pop(0)
     return(links)
 
-def downloader(links, log=False):
-    for i in links:
-        r = requests.get(i, allow_redirects=True)
+def downloader(log=False):
+    for l in urls:
+        links = pars(log=log, url=l)
+        for i in links:
+            r = requests.get(i, allow_redirects=True)
 
-        open(f"img/{i[(i.rfind('-')+1):]}", 'wb').write(r.content)
+            open(f"img/{i[(i.rfind('-')+1):]}", 'wb').write(r.content)
 
-        if log:
-            print(f"{i[(i.rfind('-')+1):]} download!")
+            if log:
+                print(f"{i[(i.rfind('-')+1):]} download!")
     return(True)
 
-#print(downloader(pars(log=True), log=True))
+#print(downloader(log=True))
 
 
