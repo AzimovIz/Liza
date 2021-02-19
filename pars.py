12b868f57935490c5cb8de68cc3860f1d6ea472e
AzimovIz <66750886+AzimovIz@users.noreply.github.com>
2021-02-19 23:10:33 +0300

    none

--- pars.py
+++ pars.py
@@ -1,9 +1,22 @@
 import requests
 import os
 from bs4 import BeautifulSoup
+import sqlite3
+
 urls = ["http://anime.reactor.cc/tag/Anime+Ero+Ass/new",
         "http://anime.reactor.cc/tag/Anime+Ero+Pantsu/new"]#,
         #"http://anime.reactor.cc/tag/Anime+Ero+Gifs/new"]
+
+img_c = sqlite3.connect(str('notes.db'))
+cursor_i = img_c.cursor()
+
+def get_old():
+    cursor_i.execute("SELECT name FROM imgs")
+    ld_list1 = cursor_i.fetchall()
+    old=[]
+    for i in ld_list1:
+        old.append(i[0])
+    return(old)
 
 def check():
 	os.chdir("img/")
@@ -21,6 +34,7 @@ def pars(log=False, url = 'http://reactor.cc'):
     item[len(item):] = item2
 
     links=[]
+
     for i in item:
         links.append(i.get('href'))
         if log:
@@ -31,13 +45,20 @@ def pars(log=False, url = 'http://reactor.cc'):
 def downloader(log=False):
     for l in urls:
         links = pars(log=log, url=l)
+
         for i in links:
             r = requests.get(i, allow_redirects=True)
+            old_list = get_old()
+            if i[(i.rfind('-')+1):] in old_list:
+                if log:
+                    print(f"{i[(i.rfind('-') + 1):]} was download last!")
+            else:
+                open(f"img/{i[(i.rfind('-')+1):]}", 'wb').write(r.content)
+                cursor_i.execute(f'INSERT INTO imgs VALUES (?)', (i[(i.rfind('-')+1):],))
+                img_c.commit()
 
-            open(f"img/{i[(i.rfind('-')+1):]}", 'wb').write(r.content)
-
-            if log:
-                print(f"{i[(i.rfind('-')+1):]} download!")
+                if log:
+                    print(f"{i[(i.rfind('-')+1):]} download!")
     return(True)
 
 #print(downloader(log=True))
